53846ee26cad99021a144b2434e569d042bb72bb
maiko254 <bonyomichael406.com>
2024-07-04 13:35:28 +0300

    A type-annotated function taking a float as argument and returning the floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module implementing a type-annotated function"""
+
+
+def floor(n: float) -> int:
+    """Function taking a float and returning the floor of the float"""
+    return int(n)
