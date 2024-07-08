daf658817cc9abafa5ff8266cb706083e9066a5f
maiko254 <bonyomichael406.com>
2024-07-08 14:02:47 +0300

    feat: Async coroutine that awaits the task_wait_random coroutine and returns a list of the delays(float values) from task_wait_random in ascending order

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" """
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Async coroutine that awaits the task_wait_random coroutine and
+    returns a list of the delays(float values) from task_wait_random in
+    ascending order
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    res = await asyncio.gather(*tasks)
+    return sorted(res)
