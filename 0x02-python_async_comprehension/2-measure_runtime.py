9a60571c8bbf59c320d9e8edd51ce61af5fb944d
maiko254 <bonyomichael406.com>
2024-07-09 13:12:05 +0300

    feat: Wrote coroutine that measures the total runtime of executing async_comprehension four times in parallel

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that measures the total runtime of a coroutine"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Coroutine that executes async_comprehension four times in parallel and
+    returns the total runtime
+    """
+    start_time = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    total_time = time.perf_counter() - start_time
+
+    return total_time
