3ed4e4aab939539ed7da43962840466e19bc09d2
maiko254 <bonyomichael406.com>
2024-07-27 12:52:20 +0300

    Implemented testcase for exception raised by access_nested_map function from utils module

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,3 +15,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, seq, expected):
         """Testcase for return value of access_nested_map method"""
         self.assertEqual(access_nested_map(nested_map, seq), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
+    ])
+    def test_access_nested_map_exception(self, nested_map, seq, keyerror):
+        """Testcase for exception raised by access_nested_map method"""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, seq)
+        self.assertEqual(str(e.exception).strip("'"), keyerror)
