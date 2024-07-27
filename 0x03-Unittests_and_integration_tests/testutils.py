445236e547396eab9c56c6d009b22ae8d13241bf
maiko254 <bonyomichael406.com>
2024-07-27 12:01:45 +0300

    Correction

--- 0x03-Unittests_and_integration_tests/testutils.py
+++ 0x03-Unittests_and_integration_tests/testutils.py
@@ -13,5 +13,4 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, seq, expected):
-        """Testcase for return value of access_nested_map method"""
         self.assertEqual(access_nested_map(nested_map, seq), expected)
